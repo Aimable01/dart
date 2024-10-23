8cdc1861cbef2ea704f92ed3215560c5905476e8
Aimable01 <aimable.kwizera14@gmail.com>
2024-10-23 19:00:03 +0200

    feat: variables and constants

---
+++ basics/variables.dart
@@ -0,0 +1,20 @@
+void main() {
+  // declare variables
+  String name = "John";
+  String address = "USA";
+  num age = 20;
+  num height = 5.9;
+  bool isMarried = false;
+
+  // print variables
+  print("Name is: $name");
+  print("Address is: $address");
+  print("Age is: $age");
+  print("Height is: $height");
+  print("Married status is: $isMarried");
+
+  // constants
+  const pi = 3.14;
+  final String nameConst = "KWIZERA Aimable";
+  print("These are constants: $pi and $nameConst");
+}
